7c9fe4dcdde30d0c52c5783adc18e9a69580330f
gautam <gautam8809240887@gmail.com>
2024-01-28 21:54:27 +0530

    add uniquevalue in input ,editbuttton and deletebutton

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
+import { useState } from 'react'
 import './App.css'
 import Todo from './Component/Todo'
 
 
 function App() {
-
-
   return (
     <section>
       <Todo />
--- src/Component/Todo.jsx
+++ src/Component/Todo.jsx
@@ -2,30 +2,62 @@ import React, { useState, useRef, useEffect } from 'react'
 
 function Todo() {
     const form = useRef();
-    const [todo, setTodo] = useState("");
+    const [todo, setTodo] = useState([]);
+    const [editTodo, setEditTodo] = useState(null)
 
     useEffect(() => {
 
     }, [todo])
 
-
     let handleTodo = (event) => {
         event.preventDefault();
-        console.log(form.current.todo.value);
-        setTodo(form.current.todo.value);
-        form.current.reset();
+        // const todoValue=form.current.todo.value
+        if (editTodo !== null) {
+            const updateTodo = [...todo];
+            updateTodo[editTodo] = form.current.todo.value;
+            setTodo(updateTodo)
+            setEditTodo(null)
+        }
+        else if (!todo.includes(form.current.todo.value)) {
+            setTodo(prevItem => [...prevItem, form.current.todo.value])
+        }
+        // form.current.todo.value = "";
     }
+    const editTodolist = (index,value) => {
+
+        setEditTodo(index,);
+
+        form.current.todo.value = todo[index];
+    };
+
+    const deleteTodo = (index) => {
+        const updateTodo = [...todo];
+        updateTodo.splice(index, 1);
+        setTodo(updateTodo);
+    };
+    const arrayDataItems = todo.map((item, index) => (
+        <li key={index}>
+            {item}
+            <button onClick={() => editTodolist(index)}>Edit</button>
+            <button onClick={() => deleteTodo(index)}>Delete</button>
+            
+        </li>));
+        console.log(arrayDataItems)
+        
 
     return (
         <section>
             <section>
                 <form ref={form}>
-                    <input type='text' name='todo' placeholder='Enter the todo' />
+                    <input type='text' name='todo'
+                        placeholder='Enter the todo' />
+
                     <button type='submit' onClick={handleTodo}>SUBMIT</button>
                 </form>
             </section>
             <section>
-//TODO :show todo list
+{/* //TODO :show todo list */}
+                <ul> new{arrayDataItems}</ul>
             </section>
         </section>
     )
