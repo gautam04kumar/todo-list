96827daa9e2e514736f08d25799373c8f24b4682
gautam <gautam8809240887@gmail.com>
2024-02-07 15:38:11 +0530

    add style

--- src/Component/Todo.jsx
+++ src/Component/Todo.jsx
@@ -35,14 +35,17 @@ function Todo() {
     const arrayDataItems = todo.map((item, index) => (
         <li key={index}
             className='mb-1 bg-gray-300
-              border-solid border-2 text p-1
+              border-solid border-2 text p-0.5  rounded-lg
           '>
             {item}
-            <button className=' ml-2  bg-white border-solid border-2  rounded-md'
-                onClick={() => editTodolist(index)}
-            >Edit</button>
+            <button className=' ml-2
+               bg-amber-500 
+               border-solid 
+               border-2 
+                rounded-md'
+                onClick={() => editTodolist(index)}>Edit</button>
 
-            <button className=' ml-2 bg-red-600 border-solid border-2   rounded-md'
+            <button className=' ml-2 bg-red-700 border-solid border-2   rounded-md'
                 onClick={() => deleteTodo(index)}>
                 Delete</button>
 
@@ -51,7 +54,7 @@ function Todo() {
 
 
     return (
-        <section>
+        <section className='place-content-center'>
             <section>
                 <form ref={form} onSubmit={handleTodo} >
                     <input className="my-5 ml-80 border-solid border-2 rounded border-slate-600 "
@@ -59,13 +62,13 @@ function Todo() {
                         type='text'
                         name='todo'
                         placeholder='Enter the todo' />
-                    <button className='bg-green-500 text-white border-solid ml-1 border-2 border-slate-700 rounded-lg'
+                    <button className='bg-slate-900 text-white border-solid ml-1 border-2 border-slate-700 rounded-lg'
                         type='submit'>
                         Submit
                     </button>
                 </form>
             </section>
-            <section className='  ml-80 w-60  flex-0'>
+            <section className='  ml-80 w-60 bg-stone-400 p-2 rounded'>
                 <ul className=''>{arrayDataItems}</ul>
             </section>
         </section>
